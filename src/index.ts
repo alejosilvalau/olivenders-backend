5cafdaf587021922d1f642a0cd29480657dc744c
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-04-27 15:21:14 -0300

    Server básico en funcionamiento

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PUERTO_POR_DEFECTO, () => {
+  console.log(`Example app listening on port ${process.env.PUERTO_POR_DEFECTO}`);
+});
