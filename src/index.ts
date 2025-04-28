47e3bbf3427518e010e3bebf459f1b752a9d35c6
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-04-28 10:36:23 -0300

    Agregado CORS

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+const allowedOrigins = ['http://localhost:4200', 'http://localhost:9876'];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      console.log('Origin:', origin);
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  })
+);
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(process.env.PUERTO_POR_DEFECTO, () => {
-  console.log(`Example app listening on port ${process.env.PUERTO_POR_DEFECTO}`);
+app.listen(process.env.DEFAULT_PORT, () => {
+  console.log(`Example app listening on port ${process.env.DEFAULT_PORT}`);
 });
