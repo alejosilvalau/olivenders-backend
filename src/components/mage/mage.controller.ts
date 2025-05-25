d8f83669c7faaf728c2e314d90a5e06754640f54
Marisol <marisolavila807@gmail.com>
2025-05-25 18:58:51 -0300

    agrego entidad Nucleo (Core) con sus propiedades basicas

--- src/components/mage/mage.controller.ts
+++ src/components/mage/mage.controller.ts
@@ -3,4 +3,15 @@ import { orm } from '../../shared/orm.js';
 import { Mage } from './mage.entity.js';
 import { z } from 'zod';
 
-const mageZodSchema = z.object({});
+const mageZodSchema = z.object({
+
+  id: z.string().uuid().optional(),
+  user: z.string().min(3, "El usuario debe tener al menos 3 caracteres"),
+  password: z.string().min(6, "La clave debe tener al menos 6 caracteres"),
+  name: z.string(),
+  surname: z.string(),
+  email: z.string().email("Debe ser un email válido"),
+  address: z.string(),
+  phone: z.string(),
+});
+
