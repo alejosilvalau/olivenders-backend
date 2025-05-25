ae1293f835ddbe633adbfcfb0ffe554a6b523d52
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-05-25 16:15:23 -0300

    Creada zod schema para mago

--- src/components/mage/mage.controller.ts
+++ src/components/mage/mage.controller.ts
@@ -3,4 +3,15 @@ import { orm } from '../../shared/orm.js';
 import { Mage } from './mage.entity.js';
 import { z } from 'zod';
 
-const mageZodSchema = z.object({});
+const mageZodSchema = z.object({
+  id: z.string().uuid().optional(),
+  username: z.string(),
+  password: z.string(),
+  name: z.string(),
+  last_name: z.string(),
+  email: z.string().email(),
+  address: z.string(),
+  phone: z.string(),
+  role: z.string(),
+  school: z.string().uuid(),
+});
