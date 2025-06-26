1c203ce6e037fccd256cf70f0f18e083a5c401a4
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-26 06:25:07 -0300

    Creada la declaración de la entidad wand

--- src/components/wand/wand.entity.ts
+++ src/components/wand/wand.entity.ts
@@ -4,7 +4,30 @@ import { BaseEntity } from '../../shared/baseEntity.entity.js';
 
 @Entity()
 export class Wand extends BaseEntity {
- 
+  @Property({ nullable: false })
+  name!: string;
 
-  
+  @Property({ nullable: false })
+  email!: string;
+
+  @Property({ nullable: false })
+  length!: string;
+
+  @Property({ nullable: false })
+  flexibility!: string;
+
+  @Property({ nullable: false })
+  description!: string;
+
+  @Property({ nullable: false })
+  state!: string;
+
+  @Property({ nullable: false })
+  image!: string;
+
+  @Property({ nullable: false })
+  profit_margin!: number;
+
+  @Property({ nullable: false })
+  total_price!: number;
 }
