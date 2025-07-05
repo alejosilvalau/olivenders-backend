6ab56fc3ef8e09706cef62cdb70716b6a51ea08a
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-07-05 19:33:01 -0300

    Creada la declaración de la entidad sale

--- src/components/sale/sale.entity.ts
+++ src/components/sale/sale.entity.ts
@@ -1,16 +1,21 @@
-import { Entity, Property, Collection, OneToMany, Cascade, types } from '@mikro-orm/core';
+import { Entity, Property, Collection, OneToMany, Cascade, types, DateTimeType } from '@mikro-orm/core';
 import { BaseEntity } from '../../shared/db/baseEntity.entity.js';
 
 @Entity()
 export class Sale extends BaseEntity {
   @Property({ nullable: false })
-  payment_id!: number;
+  payment_reference!: string;
 
-  @Property({ nullable: false, type: types.date })
-  date!: Date;
+  @Property({ nullable: false })
+  payment_provider!: string;
+  // stripe, paypal, bitcon, ethereum, wire_transfer, credit_card, debit_card
+
+  @Property({ nullable: false, type: DateTimeType })
+  date!: DateTimeType;
 
   @Property({ nullable: false })
   status!: string;
+  // pending, paid, dispatched, delivered, completed, cancelled, refunded
 
   @Property({ nullable: false })
   review!: string;
