e143281a116e006dc2683920e606e9e13c47beb7
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-07-09 21:52:03 -0300

    Modificada función de sanitización de respuesta para ser consistente con las demás

--- src/shared/entities/sanitizeAnswerResponse.ts
+++ src/shared/entities/sanitizeAnswerResponse.ts
@@ -1,10 +1,11 @@
 import { Answer } from '../../components/answer/answer.entity.js';
 import { sanitizeWizardResponse } from './sanitizeWizardResponse.js';
 import { sanitizeWandResponse } from './sanitizeWandResponse.js';
+import { wrap } from '@mikro-orm/core';
 
 export function sanitizeAnswerResponse(answer: Answer | null) {
   if (!answer) return answer;
-  const obj = { ...answer };
+  const obj = wrap(answer).toObject() as any;
   obj.wizard = sanitizeWizardResponse(answer.wizard);
   obj.wand = sanitizeWandResponse(answer.wand);
   return obj;
