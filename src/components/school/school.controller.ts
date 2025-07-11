c923cf90c186bda655c8bf6a03afba79c4188959
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-07-11 08:31:49 -0300

    Agregado paginación a school

--- src/components/school/school.controller.ts
+++ src/components/school/school.controller.ts
@@ -19,8 +19,19 @@ const sanitizeSchoolInput = sanitizeInput(schoolZodSchema);
 
 async function findAll(req: Request, res: Response, next: NextFunction) {
   try {
-    const schools = await em.find(School, {});
-    res.status(200).json({ message: 'Schools fetched', data: schools });
+    const page = Number(req.query.page) || 1;
+    const pageSize = Number(req.query.pageSize) || 10;
+    const offset = (page - 1) * pageSize;
+
+    const [schools, total] = await em.findAndCount(School, {}, { limit: pageSize, offset });
+    res.status(200).json({
+      message: 'Schools fetched',
+      data: schools,
+      total,
+      page,
+      pageSize,
+      totalPages: Math.ceil(total / pageSize),
+    });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
